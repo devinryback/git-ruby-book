620c374ebf3d003d31aba48f6bf37813d352574e
Devin Ryback <devinryback@protonmail.com>
2024-11-05 18:33:46 -0500

    Add completed exercises for 02 Basics and 03 Variables

---
+++ 03_variables/exercise_03_name_02.rb
@@ -0,0 +1,8 @@
+puts "Please enter your name:"
+
+name = gets.chomp
+puts "Hello " + name
+
+# continued...
+
+10.times { puts "#{name}" }
---
+++ 03_variables/exercise_04_name_03.rb
@@ -0,0 +1,17 @@
+puts "Enter your first name:"
+first_name = gets.chomp
+
+puts "Enter your last name:"
+last_name = gets.chomp
+
+puts "Your full name is #{first_name} #{last_name}."
+
+# Another way
+
+puts "Enter your first name:"
+first_name = gets.chomp
+
+puts "Enter your last name:"
+last_name = gets.chomp
+
+puts "Your full name is " + first_name + " " + last_name
