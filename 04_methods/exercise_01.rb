33232be02c45ddcf419ac010f7fc2dbd1fa8b0d0
Devin Ryback <devinryback@protonmail.com>
2024-11-07 11:36:06 -0500

    Add completed exercises in 04 Methods

---
+++ 04_methods/exercise_01.rb
@@ -0,0 +1,14 @@
+def greeting(name)
+  "Hello, " + name + "!"
+end
+
+puts greeting("Alex")
+
+
+### LS solution
+
+def greeting(name)
+  "Hello, " + name + ". How are you doing?"
+end
+
+puts greeting("Bob")
